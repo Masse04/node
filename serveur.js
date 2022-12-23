88e2c3445c8c7ad8af70e9ebf56ebad59967b2c1
Masse04 <massamba084@gmail.com>
2022-12-23 09:30:46 +0000

    second

--- serveur.js
+++ serveur.js
@@ -1,5 +1,5 @@
 var http = require('http') ;
 http.createServer((req,res)=> {
-    res.writeHead(200,{'Content-Type' : 'text/plain'})
+    res.writeHead(200,{'Content-Type' : 'text/html'})
     res.end('<h1>Hello Node!!!! </h1>\n')
 }).listen(3000)
